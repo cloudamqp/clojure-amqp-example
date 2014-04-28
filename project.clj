4021a925d75c365f3c2718c0b6ce7f0f39ec4089
Carl Hörberg <carl.hoerberg@gmail.com>
2014-04-28 12:10:09 +0800

    dep upgrade to langohr 2.9.0 and latest clojure

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-amqp-example "1.0.0-SNAPSHOT"
   :main clojure-amqp-example.core
   :description "Example of CloudAMQP in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.novemberain/langohr "1.0.0-beta10"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.novemberain/langohr "2.9.0"]])
